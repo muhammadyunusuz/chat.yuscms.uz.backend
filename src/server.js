9fb045f0203f3152b67614df2953764b24d7d869
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2021-06-26 18:29:52 +0500

    Add server files

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const { Server } = require('socket.io')
+
+const { PORT } = require('../config')
+
+const server = http.createServer(app)
+
+const io = new Server(server)
+
+async function start () {
+    server.listen(PORT)
+
+    
+}
+
+
+start()
